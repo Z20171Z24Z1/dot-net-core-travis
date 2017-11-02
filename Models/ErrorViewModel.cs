c3275b15496db667964d7e00f7fb210d0768d5df
Christian Farinella <christian@architected.net>
2017-11-02 11:02:49 +1100

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dot_net_core_travis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
